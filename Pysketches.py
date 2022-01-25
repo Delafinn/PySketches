b9681d115434e5c9c7e4a85071148023baa483b5
Finn <32446445+Delafinn@users.noreply.github.com>
2022-01-24 20:15:32 -0700

    PySketches.py
    added Title to Etch A Sketch

--- Pysketches.py
+++ Pysketches.py
@@ -16,6 +16,7 @@ def random_colors():
 scr = Screen()
 scr.colormode(255)
 scr.listen()
+scr.title("PySketches")
 
 
 tim = Turtle()
@@ -72,12 +73,10 @@ Hello World! Welcome to PySketches!
 Pysketches is a python etch n sketch app that has multiple colors for drawing,
 the ability to pick up the pen so you don’t have to sketch over your work.
 and the abilty to put the pen back on the canvas to continue your drawing. as well as the abilty to clear the canvas.
-
 How To
 PySketches uses several inputs. arrow keys will move the sketcher,
 “Z” will pull your pen off the canvas so you can move around the canvas without drwaing.
 “X” allows you to put the pen back on the canvas to continue drawing.
 “C” allows you the change the color (keep in mind the colors are randomly generated.
 “Spacebar” will clear the canvas.
-
 Lastly, clicking on the Canvas will exit the python program."""
